fb82e91b546dd314ae6f890337177a138010a074
Rockykid02 <144491319+Rockykid02@users.noreply.github.com>
2025-01-02 00:00:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -24,8 +24,8 @@ function convertToBool(text, fault = 'true') {
 
 //--------------------------Mr Frank--------------------------------------
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "Enter Your SubZero Session ID",
-    CAPTION: process.env.CAPTION || "*©  Gᴇɴᴇʀᴀᴛᴇᴅ Bʏ SᴜʙZᴇʀᴏ MD*",
+    SESSION_ID: process.env.SESSION_ID || "HRR1mZoR#6DgyDescmLsqN-mB1ZJRx-GswYu4wBgg7834G8YzduM",
+    CAPTION: process.env.CAPTION || "*©  Sage Bʏ Dickson MD*",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
@@ -36,7 +36,7 @@ module.exports = {
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://i.postimg.cc/m2jtqrBC/Screenshot-20241216-090753-Video-Maker.jpg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "sᴜʙᴢᴇʀᴏ ᴍᴅ ɪs ᴏɴʟɪɴᴇ🚀",
+    ALIVE_MSG: process.env.ALIVE_MSG || "By DIGITAL DYNAMO LAB 🚀",
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
@@ -44,9 +44,9 @@ module.exports = {
     AUTO_REACT: process.env.AUTO_REACT || "false",
     HEART_REACT: process.env.HEART_REACT || "false",
     OWNER_REACT: process.env.OWNER_REACT || "false",
-    BOT_NAME: process.env.BOT_NAME || "『 𝐒𝐔𝐁𝐙𝐄𝐑𝐎 𝐌𝐃 』",
+    BOT_NAME: process.env.BOT_NAME || "『 𝐒AGE DICKSON 𝐌𝐃 』",
     AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
-    AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`Status Seen By SubZero Bot 🦄 `",
+    AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`Status Seen By Sage Dickson Bot 🦄 `",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
 //--------------MADE BY MR FRANK 2024 DEC---------------->-
